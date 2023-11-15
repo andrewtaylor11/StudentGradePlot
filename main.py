c614309f7e14c681452e57287aeeccddccef21a0
andrewtaylor11 <103282704+andrewtaylor11@users.noreply.github.com>
2023-11-14 19:27:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 # main.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+import re
+import matplotlib.pyplot as plt
+import io
 
 app = Flask(__name__)
+
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
     data = request.form.get('data')
 
-    import re
-    
-    def extract_numerical_values(input_text):
-        # Use regular expression to find numerical values
-        numerical_values = re.findall(r'\b\d+\b', input_text)
-    
-        # Convert the values to integers and return as a list
-        numerical_list = [int(value) for value in numerical_values]
-    
-        return numerical_list
-    
-    
-    
-    
-    import matplotlib.pyplot as plt
-    
-    # Define the raw percentage data
+def extract_numerical_values(input_text):
+    numerical_values = re.findall(r'\b\d+\b', input_text)
+    numerical_list = [int(value) for value in numerical_values]
+    return numerical_list
+
+
     percentages = extract_numerical_values(data)
-    
-    
-    
-    
-    # Define grade categories and corresponding percentage ranges
+
     grade_categories = [
         (95.01, 100, 'A+'),
         (85.01, 95, 'A'),
@@ -45,74 +34,44 @@ def process_data():
         (50.01, 53, 'D-'),
         (0, 50, 'F')
     ]
-    
-    # Initialize gradeCount hash map
+
     gradeCount = {
-        'F': 0,
-        'D-': 0,
-        'D': 0,
-        'D+': 0,
-        'C-': 0,
-        'C': 0,
-        'C+': 0,
-        'B-': 0,
-        'B': 0,
-        'B+': 0,
-        'A-': 0,
-        'A': 0,
-        'A+': 0,
+        'F': 0, 'D-': 0, 'D': 0, 'D+': 0, 'C-': 0, 'C': 0,
+        'C+': 0, 'B-': 0, 'B': 0, 'B+': 0, 'A-': 0, 'A': 0, 'A+': 0,
     }
-    
-    # Iterate over percentages and update gradeCount
+
     for percentage in percentages:
         for grade_range in grade_categories:
             min_range, max_range, grade = grade_range
             if min_range <= percentage <= max_range:
                 gradeCount[grade] += 1
-                break  # Break to avoid updating multiple grades for the same percentage
-    
-    
-    
-    # Define grade colors
+                break
+
     grade_colors = {
-        'A+': 'blue',
-        'A': 'green',
-        'A-': 'lime',
-        'B+': 'cyan',
-        'B': 'yellow',
-        'B-': 'orange',
-        'C+': 'purple',
-        'C': 'pink',
-        'C-': 'magenta',
-        'D+': 'brown',
-        'D': 'red',
-        'D-': 'maroon',
+        'A+': 'blue', 'A': 'green', 'A-': 'lime', 'B+': 'cyan',
+        'B': 'yellow', 'B-': 'orange', 'C+': 'purple', 'C': 'pink',
+        'C-': 'magenta', 'D+': 'brown', 'D': 'red', 'D-': 'maroon',
         'F': 'black'
     }
-    
+
     grades = list(gradeCount.keys())
     frequency = list(gradeCount.values())
-    
-    # Create a bar plot
+
     plt.bar(grades, frequency, color=[grade_colors[grade] for grade in grades])
-    
-    # Adding labels and title
     plt.xlabel('Letter Grade')
     plt.ylabel('Frequency')
     plt.title('Distribution of Letter Grades')
-    
-    # Display the frequency on top of each bar
+
     for i, v in enumerate(frequency):
         plt.text(i, v + 0.1, str(v), ha='center', va='bottom')
-    
-    # Show the plot
-    plt.show()
 
+    # Save the plot as an image in memory
     img_bytes = io.BytesIO()
     plt.savefig(img_bytes, format='png')
     img_bytes.seek(0)
     plt.close()
 
+    # Return the image file
     return send_file(img_bytes, mimetype='image/png')
 
 if __name__ == '__main__':
